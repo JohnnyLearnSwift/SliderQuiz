c3dfc317e6afc129f6ca601a2184ef19a3544dd0
JohnyLearnSwift <eugenietyan@gmail.com>
2022-09-19 15:03:34 +0300

    Initial Commit

---
+++ SliderQuiz/SliderQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderQuizApp.swift
+//  SliderQuiz
+//
+//  Created by Eugenie Tyan on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
